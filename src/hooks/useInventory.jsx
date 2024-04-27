fcd5fb1a2f452581e4d74fa4733f21ad1c9ad87b
Autumn Valenta <autumn@discoverywritten.com>
2024-04-26 18:54:14 -0600

    Save/load equipment directly

--- src/hooks/useInventory.jsx
+++ src/hooks/useInventory.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import { parseInventory, unparseInventory } from '../utils';
+import useSave from './useSave';
 
 const CATEGORIES = [
   'hair',
@@ -14,69 +14,34 @@ const CATEGORIES = [
   'shield',
 ];
 
-function getInventoryType(subject, kind) {
-  const saveSlot = localStorage.getItem('latest');
-  const key = `${saveSlot}/${subject}/inventory/${kind}`;
-  const itemsString = localStorage.getItem(key) || '';
-  const items = parseInventory({ inventory: itemsString });
-  return items;
-}
-
-function setInventoryType(subject, kind, items) {
-  const saveSlot = localStorage.getItem('latest');
-  const key = `${saveSlot}/${subject}/inventory/${kind}`;
-  const itemsString = unparseInventory(kind, items);
-  localStorage.setItem(key, itemsString);
-}
-
-function getEquipmentType(subject, kind) {
-  const saveSlot = localStorage.getItem('latest');
-  const key = `${saveSlot}/${subject}/equipped/${kind}`;
-  const id = localStorage.getItem(key);
-  return parseInt(id, 10) || null;
-};
-
-function setEquipmentType(subject, kind, id) {
-  const saveSlot = localStorage.getItem('latest');
-  const key = `${saveSlot}/${subject}/equipped/${kind}`;
-  localStorage.setItem(key, id);
-};
-
 export default function useInventory(subject) {
-  const [inventory, setInventory] = useState({});
-  const [equipment, setEquipment] = useState({});
+  const [inventory, setInventory] = useState(
+    Object.fromEntries(CATEGORIES.map((category) => [category, []]))
+  );
+  const [equipment, setEquipment] = useState(
+    Object.fromEntries(CATEGORIES.map((category) => [category, null]))
+  );
 
+  useSave({
+    [`${subject}/inventory`]: [inventory, setInventory],
+    [`${subject}/equipment`]: [equipment, setEquipment],
+  });
+
+  // Player responds to Buy event
   useEffect(() => {
-    // respond to Buy event
+    if (subject !== 'player') return;
     const buy = function({ detail: {kind, ...item} }) {
       acquire(kind, item);  // fixme: bought items don't have an id
     };
     window.addEventListener('Buy', buy);
     return () => window.removeEventListener('Buy', buy);
-  });
-
-  useEffect(() => {
-    const inventory = Object.fromEntries(CATEGORIES.map((category) => [category, []]));
-    const equipment = Object.fromEntries(CATEGORIES.map((category) => [category, null]));
-    CATEGORIES.forEach((kind) => {
-      const items = getInventoryType(subject, kind);
-      const equipped = getEquipmentType(subject, kind);
-      inventory[kind].push(...items);
-      equipment[kind] = inventory[kind].find(({ id }) => id === equipped)?.id || null;
-    });
-    setInventory(inventory);
-    setEquipment(equipment);
   }, [subject]);
 
   const equip = function(kind, id) {
-    setEquipment((equipment) => {
-      const newEquipment = {
+    setEquipment((equipment) => ({
         ...equipment,
         [kind]: id,
-      };
-      setEquipmentType(subject, kind, newEquipment[kind]);
-      return newEquipment;
-    });
+      }));
   };
 
   const acquire = function(kind, item) {
@@ -85,14 +50,10 @@ export default function useInventory(subject) {
       (max, { id=0 }) => Math.max(max, id),
       0
     );
-    setInventory((inventory) => {
-      const newInventory = {
+    setInventory((inventory) => ({
         ...inventory,
         [kind]: [...inventory[kind], { ...item, id: maxId + 1 }],
-      };
-      setInventoryType(subject, kind, newInventory[kind])
-      return newInventory;
-    });
+      }));
   };
 
   return {
