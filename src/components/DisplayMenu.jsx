e02b5dbab9995ff12d651d18e2454c3bd42e631c
Autumn Valenta <autumn@discoverywritten.com>
2024-04-30 19:33:31 -0600

    Drop erroneous 'width' useEffect requirement

--- src/components/DisplayMenu.jsx
+++ src/components/DisplayMenu.jsx
@@ -164,7 +164,7 @@ export default function DisplayMenu({
     };
     window.addEventListener('keydown', keyHandler);
     return () => window.removeEventListener('keydown', keyHandler);
-  }, [options, width, _viewportHeight]);
+  }, [options, _viewportHeight]);
 
   // Key release handler for Use key
   useEffect(() => {
