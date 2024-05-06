189064d670fb046765a9160f2d0da8d4598f0325
Autumn Valenta <autumn@discoverywritten.com>
2024-05-05 22:38:35 -0600

    Store map layers instead of crunching every frame

--- src/hooks/useLocation.jsx
+++ src/hooks/useLocation.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import usePosition from './usePosition';
 import useWorld from './useWorld';
 
@@ -9,25 +11,39 @@ export default function useLocation({ world, x, y, width, height, possesses, key
     keyMap,
   });
 
-  const localX = posX % w;
-  const localY = posY % h;
+  const [area, setArea] = useState([]);
+  const [solid, setSolid] = useState([]);
+  const [passable, setPassable] = useState([]);
+  const [objects, setObjects] = useState([]);
+
+  const localX = posX % width;
+  const localY = posY % height;
   const originX = posX - localX;
   const originY = posY - localY;
 
-  const area = map.slice(originY, originY + h).map(
-    (row) => row.slice(originX, originX + w)
-  );
-  const solid = area.map((row) => row.map((cell) => walls.includes(cell) ? cell : ' '));
-  const passable = area.map((row) => row.map((cell) => walls.includes(cell) ? ' ' : cell));
-  const objects = Array.from({ length: h }, () => ' '.repeat(w).split(''));
-  Object.entries(interactions).forEach(([location]) => {
-    let [ly, lx] = location.split(',').map(Number);
-    ly--; lx--;
-    if (lx >= originX && lx < originX + w && ly >= originY && ly < originY + h) {
-      objects[ly - originY][lx - originX] = map[ly][lx];
-    }
-  });
-  objects[localY][localX] = marker;
+  useEffect(() => {
+    setArea(map.slice(originY, originY + height).map(
+      (row) => row.slice(originX, originX + width)
+    ));
+  }, [map, originX, originY]);
+
+  useEffect(() => {
+    setSolid(area.map((row) => row.map((cell) => walls.includes(cell) ? cell : ' ')));
+    setPassable(area.map((row) => row.map((cell) => walls.includes(cell) ? ' ' : cell)));
+  }, [area, interactions, height, width]);
+
+  useEffect(() => {
+    const objects = Array.from({ length: height }, () => ' '.repeat(width).split(''));
+    Object.entries(interactions).forEach(([location]) => {
+      let [ly, lx] = location.split(',').map(Number);
+      ly--; lx--;
+      if (lx >= originX && lx < originX + width && ly >= originY && ly < originY + height) {
+        objects[ly - originY][lx - originX] = map[ly][lx];
+      }
+    });
+    objects[localY][localX] = marker;
+    setObjects(objects);
+  }, [map, interactions, posX, posY, marker, width, height]);
 
   return {
     layers: {
