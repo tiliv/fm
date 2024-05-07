55cd1be0dacd7e7258f5784f06ec496f11daa241
Autumn Valenta <autumn@discoverywritten.com>
2024-05-06 18:06:13 -0600

    Move loadSprite to utils

--- src/hooks/useSpriteLayers.jsx
+++ src/hooks/useSpriteLayers.jsx
@@ -1,12 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-
-const RARITY_COLORS = [
-  '#888',
-  '#c7c7c7',
-  '#8d8',
-  '#ff0',
-  '#f0f',
-];
+import { RARITY_COLORS, loadSprite } from '../utils';
 
 const MAP_KIND_SAME = (kind) => kind;
 
@@ -70,42 +63,4 @@ export default function useSpriteLayers({
     sprites,
     layers,
   };
-}
-
-async function loadSprite(kind, item, { offsetLeft=0, width, height }, ...positions) {
-  const { template='none', rarity=0, name='--' } = item;
-  const [[row, col], secondPosition=null] = positions;
-  return await fetch(`equipment/${kind}/${template}.txt?${item.kind}`)
-    .then((res) => res.text())
-    .then((text) => {
-      const buffer = Array.from({ length: height }, () => Array(offsetLeft + width).fill(' '));
-      const [graphics, stats] = text.trim().split('---');
-      const lines = graphics.split('\n');
-      let icon = null;
-      lines.forEach((line) => {
-        let [offset, sprites=''] = line.split(':');
-        offset = parseInt(offset, 10);
-        if (!secondPosition || !sprites.includes(',')) {
-          sprites = sprites.split('');
-          sprites.forEach((char, i) => {
-            buffer[row + offset][col + i + offsetLeft] = char;
-          });
-        } else if (sprites.includes(',')) {
-          sprites = sprites.split(',', 2);
-          buffer[row + offset][col + offsetLeft] = sprites[0];
-          buffer[secondPosition[0] + offset][secondPosition[1] + offsetLeft] = sprites[1];
-        }
-        if (!icon) {
-          icon = sprites.join('').trim()[0];
-        }
-    });
-      const data = {
-        ...item, template, rarity, name, buffer, icon,
-      };
-      stats && stats.split('\n').forEach((line) => {
-        const [stat, value] = line.split(':');
-        data.stats[stat] = parseInt(value, 10);
-      });
-      return data;
-    });
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
+export const RARITY_COLORS = [
+  '#888',
+  '#c7c7c7',
+  '#8d8',
+  '#ff0',
+  '#f0f',
+];
+
 const KEY_ALIASES = {
   ArrowDown: '↓',
   ArrowUp: '↑',
@@ -58,4 +66,44 @@ export function bufferizeList(topMargin, buffer, width, height, scrollOffset) {
     .slice(scrollOffset, scrollOffset + height - topMargin);
   scrolledBuffer.unshift(...Array(topMargin).fill(''));
   return scrolledBuffer;
+}
+
+
+export async function loadSprite(kind, item, { offsetLeft=0, width, height }, ...positions) {
+  const { template='none', rarity=0, name='--' } = item;
+  const [[row, col], secondPosition=null] = positions;
+  return await fetch(`equipment/${kind}/${template}.txt?${item.kind}`)
+    .then((res) => res.text())
+    .then((text) => {
+      const buffer = Array.from({ length: height }, () => Array(offsetLeft + width).fill(' '));
+      const [graphics, stats] = text.trim().split('---');
+      const lines = graphics.split('\n');
+      let icon = null;
+      lines.forEach((line) => {
+        let [offset, sprites=''] = line.split(':');
+        offset = parseInt(offset, 10);
+        if (!secondPosition || !sprites.includes(',')) {
+          sprites = sprites.split('');
+          sprites.forEach((char, i) => {
+            buffer[row + offset][col + i + offsetLeft] = char;
+          });
+        } else if (sprites.includes(',')) {
+          sprites = sprites.split(',', 2);
+          buffer[row + offset][col + offsetLeft] = sprites[0];
+          buffer[secondPosition[0] + offset][secondPosition[1] + offsetLeft] = sprites[1];
+        }
+        if (!icon) {
+          icon = sprites.join('').trim()[0];
+        }
+    });
+      // console.log('loadSprite', kind, template, icon);
+      const data = {
+        ...item, template, rarity, name, buffer, icon,
+      };
+      stats && stats.split('\n').forEach((line) => {
+        const [stat, value] = line.split(':');
+        data.stats[stat] = parseInt(value, 10);
+      });
+      return data;
+    });
 }
